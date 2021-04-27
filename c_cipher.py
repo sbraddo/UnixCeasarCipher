28a837e14727bf4e40bbdbd5a4282c4cc06df81f
sbraddo <81383505+sbraddo@users.noreply.github.com>
2021-04-26 22:50:12 -0400

    Update c_cipher.py

--- c_cipher.py
+++ c_cipher.py
@@ -1,1 +1,21 @@
 #CAESAR CIPHER CODE PYTHON
+def caesar_cipher(shift):
+  count = 0
+  count2 = 0
+  m_str = str(input("Enter a message. ")
+  c_str = ""
+  u_str = m_str.upper()
+  for i in u_str:
+    temp = ord(i)
+    temp_char = chr((temp + shift) % 26)
+    if count >= 5:
+       c_str = c_str + " " + temp_char
+       count = 0
+    else:
+       c_str = c_str + temp_char
+       #count = count + 1
+    if count2 >= 10:
+       c_str = c_str + "\n"
+    count = count + 1
+    count2 = count2 + 1
+ return c_str
